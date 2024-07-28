12b1679885a610c2b1a76d715f3488da1748e37a
Bijay Bikram <becauseiam33@gmail.com>
2024-07-28 21:56:31 +0545

    reducer implemented for updation of cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import { RouterProvider } from 'react-router-dom'
+import { BrowserRouter, RouterProvider, Routes, Route } from 'react-router-dom'
 import './App.css'
 import router from './routes'
 import Navbar from './globals/components/navbar/Navbar'
 import Footer from './globals/components/footer/Footer'
 import { Provider } from 'react-redux'
 import store from './store/store'
+import Home from './pages/home/Home'
+import Login from './pages/auth/login/Login'
+import Cart from './pages/cart/Cart'
 
 function App() {
 
   return (
     <>
       <Provider store={store}>
-      <Navbar/>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/cart" element={<Cart/>} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* <Navbar/>
+<RouterProvider router={router} /> */}
       </Provider>
     </>
+
+
   )
 }
 
--- src/globals/components/navbar/Navbar.jsx
+++ src/globals/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
 
 export default function Navbar() {
+    const navigate = useNavigate()
     const items = useSelector((state)=>state.cart)
     // console.log(items)
   return (
@@ -8,7 +10,7 @@ export default function Navbar() {
             <div className="container m-auto px-2 md:px-12 lg:px-7">
                 <div className="flex flex-wrap items-center justify-between py-3 gap-6 md:py-4 md:gap-0">
                     <div className="w-full px-6 flex justify-between lg:w-max md:px-0">
-                        <a href="https://tailus.io/blocks/hero-section" aria-label="logo" className="flex space-x-2 items-center">
+                        <a href="/" onClick={()=> navigate("/")} aria-label="logo" className="flex space-x-2 items-center">
                             <img src="https://tailus.io/sources/blocks/food-delivery/preview/images/icon.png" className="w-12" alt="tailus logo" width="144" height="133"/>
                             <span className="text-2xl font-bold text-yellow-900">Online <span className="text-yellow-700">MOMO</span></span>
                         </a>
@@ -33,7 +35,7 @@ export default function Navbar() {
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="#" className="block md:px-4 transition hover:text-yellow-700">
+                                    <a href="#" onClick={() => navigate("/cart")} className="block md:px-4 transition hover:text-yellow-700">
     <span>Cart<sup>{items.length}</sup></span>
                                     </a>
                                 </li>

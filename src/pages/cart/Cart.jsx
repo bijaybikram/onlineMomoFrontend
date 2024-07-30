25eb30936a4d6eee5ee7f89d8925825f3fff8e9a
Bijay Bikram <becauseiam33@gmail.com>
2024-07-31 00:20:52 +0545

    redux-thunk and api implemented for product fetching

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { remove } from '../../store/cartSlice'
 
 const Cart = () => {
+    const dispatch = useDispatch()
     const products = useSelector((state)=> state.cart)
-    console.log(products)
+    // console.log(products)
+    const removeItem = (productId) => {
+        dispatch(remove(productId))
+        console.log("Item deleted")
+    }
   return (
     <>
         
@@ -25,13 +31,13 @@ const Cart = () => {
                 <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                 <div className="flex items-center border-gray-100">
                     <span className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"> - </span>
-                    <input className="h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value="2" min="1" />
+                    <input className="h-8 w-8 border bg-white text-center text-xs outline-none" type="number" defaultValue="2" min="1" />
                     <span className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"> + </span>
                 </div>
                 <div className="flex items-center space-x-4">
                     <p className="text-sm">{product.productPrice}</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    <svg onClick={()=>removeItem(product._id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </div>
                 </div>
--- src/pages/home/components/Product.jsx
+++ src/pages/home/components/Product.jsx
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import Footer from '../../../globals/components/footer/Footer'
-import axios from 'axios'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { add } from '../../../store/cartSlice'
+import { fetchProducts } from '../../../store/productSlice'
 
 const Product = () => {
-    const [products, setProducts] = useState([])
     const dispatch = useDispatch()
 
-    const fetchProducts = async()=> {
-        const response = await axios.get("http://localhost:3000/api/products")
-        if(response.status == 200){
-            setProducts(response.data.data)
-        }
-    }
+    const {data: products, status} = useSelector((state)=> state.product)
     useEffect(()=> {
-        fetchProducts()
+        dispatch(fetchProducts())
     },[])
 
     const addToCart = (product) => {
         dispatch(add(product))
+    }
+
+    if(status == "loading"){
+       return <h1>Loading...</h1>
+    }
+    if(status == "error"){
+       return <h1>Error! Something went wrong.</h1>
     }
   return (
     <>
@@ -29,20 +30,20 @@ const Product = () => {
             <div className="container m-auto px-6 pt-32 md:px-12 lg:pt-[4.8rem] lg:px-7">
         {/* <!-- component --> */}
         <h1 className="font-bold text-4xl text-yellow-900 md:text-2xl lg:w-10/12">Our Popular Foods</h1>
-        <div class="flex flex-row flex-wrap -mx-2">  
+        <div className="flex flex-row flex-wrap -mx-2">  
         {products.map((product) => {
             return(
-                <div key={product._id} class="mx-auto mt-11 w-80 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
-                <img class="h-48 w-full object-cover object-center" src={product.productImage} alt="Product Image" />
-                <div class="p-4">
-                    <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900">{product.productName}</h2>
-                    <p class="mb-2 text-base dark:text-gray-300 text-gray-700">{product.productDescription}</p>
-                    <div class="flex items-center">
-                    <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">Rs.{product.productPrice}</p>
-                    {/* <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">$25.00</p>
-                    <p class="ml-auto text-base font-medium text-green-500">20% off</p> */}
-                    <p class="text-base  font-medium text-gray-500 dark:text-gray-300">{product.productStatus}</p>
-                    <button onClick={() => {addToCart(product)}} class="px-4 py-2 bg-blue-800 mx-6 text-white text-xs font-bold uppercase rounded">Add to Cart</button>
+                <div key={product._id} className="mx-auto mt-11 w-80 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+                <img className="h-48 w-full object-cover object-center" src={product.productImage} alt="Product Image" />
+                <div className="p-4">
+                    <h2 className="mb-2 text-lg font-medium dark:text-white text-gray-900">{product.productName}</h2>
+                    <p className="mb-2 text-base dark:text-gray-300 text-gray-700">{product.productDescription}</p>
+                    <div className="flex items-center">
+                    <p className="mr-2 text-lg font-semibold text-gray-900 dark:text-white">Rs.{product.productPrice}</p>
+                    {/* <p className="text-base  font-medium text-gray-500 line-through dark:text-gray-300">$25.00</p>
+                    <p className="ml-auto text-base font-medium text-green-500">20% off</p> */}
+                    <p className="text-base  font-medium text-gray-500 dark:text-gray-300">{product.productStatus}</p>
+                    <button onClick={() => {addToCart(product)}} className="px-4 py-2 bg-blue-800 mx-6 text-white text-xs font-bold uppercase rounded">Add to Cart</button>
                 </div>
                 </div>
                 </div>

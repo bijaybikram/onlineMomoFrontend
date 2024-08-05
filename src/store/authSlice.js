2d2958f0c86c515055f87d2f8a8731af101892e2
Bijay Bikram <becauseiam33@gmail.com>
2024-08-05 12:35:16 +0545

    register API integrated

---
+++ src/store/authSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { STATUSES } from "../globals/misc/Statuses";
+import axios from "axios";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    data: [],
+    status: STATUSES.LOADING,
+  },
+  reducers: {
+    setUser(state, action) {
+      state.data = action.payload;
+    },
+    setStatus(state, action) {
+      state.status = action.payload;
+    },
+  },
+});
+
+export const { setUser, setStatus } = authSlice.actions;
+
+export default authSlice.reducer;
+
+export function registerUser(data) {
+  return async function loginUserThunk(dispatch) {
+    dispatch(setStatus(STATUSES.LOADING));
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/registfer",
+        data
+      );
+      dispatch(setUser(response.data.data));
+      dispatch(setStatus(STATUSES.SUCCESS));
+    } catch (error) {
+      console.log(error);
+      dispatch(setStatus(STATUSES.ERROR));
+    }
+  };
+}
--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,11 +1,6 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-
-const STATUSES = Object.freeze({
-  SUCCESS: "success",
-  ERROR: "error",
-  LOADING: "loading",
-});
+import { STATUSES } from "../globals/misc/Statuses";
 
 const productSlice = createSlice({
   name: "product",
@@ -21,41 +16,41 @@ const productSlice = createSlice({
       state.status = action.payload;
     },
   },
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchProducts.pending, (state, action) => {
-        state.status = STATUSES.LOADING;
-      })
-      .addCase(fetchProducts.fulfilled, (state, action) => {
-        state.status = STATUSES.SUCCESS;
-        state.data = action.payload;
-      })
-      .addCase(fetchProducts.rejected, (state, action) => {
-        state.status = STATUSES.ERROR;
-      });
-  },
+  // extraReducers: (builder) => {
+  //   builder
+  //     .addCase(fetchProducts.pending, (state, action) => {
+  //       state.status = STATUSES.LOADING;
+  //     })
+  //     .addCase(fetchProducts.fulfilled, (state, action) => {
+  //       state.status = STATUSES.SUCCESS;
+  //       state.data = action.payload;
+  //     })
+  //     .addCase(fetchProducts.rejected, (state, action) => {
+  //       state.status = STATUSES.ERROR;
+  //     });
+  // },
 });
 
 export const { setProducts, setStatus } = productSlice.actions;
 
 export default productSlice.reducer;
 
-export const fetchProducts = createAsyncThunk("products/fetch", async () => {
-  const response = await axios.get("http://localhost:3000/api/products");
-  const { data } = response.data;
-  return data;
-});
+// export const fetchProducts = createAsyncThunk("products/fetch", async () => {
+//   const response = await axios.get("http://localhost:3000/api/products");
+//   const { data } = response.data;
+//   return data;
+// });
 
-// export function fetchProducts() {
-//   return async function fetchProductThunk(dispatch, getState) {
-//     dispatch(setStatus(STATUSES.LOADING));
-//     try {
-//       const response = await axios.get("http://localhost:3000/api/products");
-//       dispatch(setProducts(response.data.data));
-//       dispatch(setStatus(STATUSES.SUCCESS));
-//     } catch (error) {
-//       console.log(error);
-//       dispatch(setStatus(STATUSES.ERROR));
-//     }
-//   };
-// }
+export function fetchProducts() {
+  return async function fetchProductThunk(dispatch, getState) {
+    dispatch(setStatus(STATUSES.LOADING));
+    try {
+      const response = await axios.get("http://localhost:3000/api/products");
+      dispatch(setProducts(response.data.data));
+      dispatch(setStatus(STATUSES.SUCCESS));
+    } catch (error) {
+      console.log(error);
+      dispatch(setStatus(STATUSES.ERROR));
+    }
+  };
+}
